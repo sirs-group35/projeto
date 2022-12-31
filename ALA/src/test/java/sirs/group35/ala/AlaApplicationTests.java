145fcdcd5716bab669d05fb5686ddbaf4c6117c3
masterix0 <andregoncalves2001@hotmail.com>
2022-12-31 01:47:26 +0000

    First mk2 commit

---
+++ ALA/src/test/java/sirs/group35/ala/AlaApplicationTests.java
@@ -0,0 +1,13 @@
+package sirs.group35.ala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
