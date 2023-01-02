cc5ae10400ead72dc128550c83ecb23ab2ff2a23
masterix0 <andregoncalves2001@hotmail.com>
2023-01-02 01:13:32 +0000

    Fixed missing slashes

--- ALA/src/main/java/sirs/group35/ala/web/MainController.java
+++ ALA/src/main/java/sirs/group35/ala/web/MainController.java
@@ -6,17 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("Firm_Organization")
+    @GetMapping("/Firm_Organization")
     public String organization() {
         return "organization";
     }
 
-    @GetMapping("Firm_Contacts")
+    @GetMapping("/Firm_Contacts")
     public String contacts() {
         return "contacts";
     }
 
-    @GetMapping("Areas_Of_Activity")
+    @GetMapping("/Areas_Of_Activity")
     public String areasOfActivity() {
         return "areas";
     }
