1fb27bb5796744d1ced5f0180651172acd246b09
masterix0 <andregoncalves2001@hotmail.com>
2023-01-05 08:01:07 +0000

    Updated navbar

--- ALA/src/main/java/sirs/group35/ala/service/UserService.java
+++ ALA/src/main/java/sirs/group35/ala/service/UserService.java
@@ -1,8 +1,8 @@
 package sirs.group35.ala.service;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
-import sirs.group35.ala.model.User;
 import sirs.group35.ala.model.Lawyer;
+import sirs.group35.ala.model.User;
 import sirs.group35.ala.web.dto.UserRegistrationDto;
 
 public interface UserService extends UserDetailsService {
